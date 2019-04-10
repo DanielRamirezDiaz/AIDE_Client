52de8191760f103b1c55a209400f75482286f7ae
Daniel Ramirez <DanielRamirezDiaz@github.com>
2019-04-10 16:50:45 -0700

    Update
    For now, the API returns a simplified list of all the processes in the computer.

---
+++ AIDE_Client/Controllers/ProcessesController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AIDE_Client.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProcessesController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<IEnumerable<Models.Process>> GetImportantProcesses()
+        {
+            try
+            {
+                var ie = Get();
+
+                return Ok(ie);
+            }
+            catch(Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+
+        }
+
+        //---------- private ----------
+
+        private List<Models.Process> Get()
+        {
+            var badList = System.Diagnostics.Process.GetProcesses();
+
+            List<Models.Process> goodList = new List<Models.Process>();
+
+            foreach(System.Diagnostics.Process p in badList)
+            {
+                var name = p.ProcessName;
+                //if (name.Equals("update.exe"))
+                    goodList.Add(new Models.Process
+                    {
+                        Name = name,
+                        PID = p.Id,
+                        RespondingState = p.Responding,
+                        Memory = (int)p.PagedMemorySize64
+                    });
+            }
+
+            goodList = goodList.OrderBy(Process => Process.Name).ToList(); ;
+
+            return goodList;
+        }
+
+    }
+}
---
+++ AIDE_Client/Models/Process.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AIDE_Client.Models
+{
+    public class Process
+    {
+        public string Name { get; set; }
+
+        public int PID { get; set; }
+
+        public bool RespondingState { get; set; }
+
+        public int Memory { get; set; }
+    }
+}
