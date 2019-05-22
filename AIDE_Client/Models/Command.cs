780eba4fe2b984ee7030cf3b7d7b388a1f0111eb
Daniel Ramirez <DanielRamirezDiaz@outlook.com>
2019-05-22 13:11:46 -0700

    Create Command.cs

---
+++ AIDE_Client/Models/Command.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AIDE_Client.Models
+{
+    public class Command
+    {
+        public string FileName { get; set; }
+
+        public string Arguments { get; set; }
+
+        public int TimeBetweenChecks { get; set; }
+
+        public int Times { get; set; }
+    }
+}
